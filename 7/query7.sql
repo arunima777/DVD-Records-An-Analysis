c4f76acd84cbe9527f60904cc63b9fed7536f040
Arunima <138151351+arunima777@users.noreply.github.com>
2023-10-18 22:10:23 -0400

    Update query7.sql

--- 7/query7.sql
+++ 7/query7.sql
@@ -1,7 +1,9 @@
+------Different movie genres and their average rental duration-----------------
+
 select name as movie_genre, avg (rental_rate) as avg_rent_rate from category
 join film_category using (category_id)
 join film using (film_id)
 group by 1
 order by 2 desc
---different movie genrea and their avergae rental duration 7
+
 
