ec72fca43ad50dc24d6ef625ce0e922f1c0e4737
Arunima <138151351+arunima777@users.noreply.github.com>
2023-10-18 22:08:58 -0400

    Update query4.sql

--- 4/query4.sql
+++ 4/query4.sql
@@ -1,6 +1,7 @@
+---Listing special features types like trailers, deleted scenes,behind the scenes and count of films with these type of features--------
+
 SELECT special_features, COUNT(*) AS film_count
 FROM film
 GROUP BY special_features
----Listing special features types like trailers, deleted scenes,
---behind the scenes and count of films with these type of features 4
+
 
