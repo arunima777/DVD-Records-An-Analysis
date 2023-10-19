49dae535d1dff457a0788391e35e04a5df711b3a
Arunima <138151351+arunima777@users.noreply.github.com>
2023-10-18 22:09:22 -0400

    Update query5.sql

--- 5/query5.sql
+++ 5/query5.sql
@@ -1,5 +1,8 @@
+------------------Description having word scientist and having replacement cost upto 10 and their rating-----
+
+
 select title, description, replacement_cost, rating
 from film
 where description ilike '%scientist%' and replacement_cost < 10
----description having word scientist and having replacement cost upto 10 and their rating 5
+
 
