a217c8aa58513046ab4c5ba23c9f94338c60f80d
Arunima <138151351+arunima777@users.noreply.github.com>
2023-10-18 22:10:52 -0400

    Update query8.sql

--- 8/query8.sql
+++ 8/query8.sql
@@ -1,9 +1,11 @@
+------------------Country with maximum customers renting dvd's----------------------------
+
 select country, count (customer_id) as total_count_customers from country
 inner join city on country.country_id = city.country_id
 inner join address on city.city_id = address.city_id
 inner join customer on address.address_id = customer.address_id
 group by 1
 order by 2 desc
---country with maximum customers renting dvd's 8
+
 
 
