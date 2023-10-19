ec19e4ad4b82729581e1c8cf201f63418e30639e
Arunima <138151351+arunima777@users.noreply.github.com>
2023-10-18 22:06:21 -0400

    Update query10.sql

--- 10/query10.sql
+++ 10/query10.sql
@@ -1,8 +1,10 @@
- select first_name, last_name, count (film_actor.film_id) from film_actor join actor
+------------------Actors acting in more than 30 films, limit 10 -------------
+
+select first_name, last_name, count (film_actor.film_id) from film_actor join actor
  on film_actor.actor_id = actor.actor_id
  group by ( first_name, last_name)
  having count (film_actor.film_id)>30
  limit 10
----actors acting in more than 30 films, limit 10     10
+    10
  
 
