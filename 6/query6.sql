f6657f97f30c1f8f6735b6d3e359d78de3296b4d
Arunima <138151351+arunima777@users.noreply.github.com>
2023-10-18 22:09:52 -0400

    Update query6.sql

--- 6/query6.sql
+++ 6/query6.sql
@@ -1,8 +1,10 @@
+-----display 5 rows with title, length and description from two tables actor, film ------------------
+
 select f.description, f.title, f.length 
 from film_actor ac
 join film f on (
 f.film_id = ac.film_id
 )
 limit 5
----5 rows with title, length and description from two tables actor, film 6
+
 
